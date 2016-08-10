30f04477af3dfe17da9a739884d824a78b840ef2
iemharsh <harsh.wadhwa.182@gmail.com>
2016-08-10 23:06:11 +0530

    Array Sorting from last value

---
+++ Array.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+
+int main() {
+
+    char a[][10] = {"abc", "cra", "bcd"};
+    char t[20];
+    int i, j, o ,p;
+    int size = sizeof(a)/sizeof(a[0]);
+    
+    for(i = 0; i < size - 1; i++) {
+        o = strlen(a[i]) - 1;
+        for(j = i + 1; j < size; j++) {
+           p = strlen(a[j]) - 1;     
+            if(a[i][o] > a[j][p]) {
+                strcpy(t, a[i]);
+                strcpy(a[i], a[j]);
+                strcpy(a[j], t);
+                break;
+            }
+
+        }
+    }
+
+    for(i = 0; i < 3; i++) {
+        printf("%s\n", a[i]);
+    }
+
+    
+
+    
+    return 0;
+}
